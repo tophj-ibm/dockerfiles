99a7c5aeedb190f02974819fb4cb4a56535484ab
Christopher Jones <tophj@linux.vnet.ibm.com>
2015-11-17 16:52:07 -0600

    Removed unused build commands

--- sametime/OSX/waitForStart.sh
+++ sametime/OSX/waitForStart.sh
@@ -2,10 +2,10 @@
 
 /usr/bin/sametime80
 count=0
-while [ $count -le 30 ]
+while [ $count -le 6 ]
 do
 	case "$(pidof sametime | wc -w)" in
-	#container is alive, because container will normally exit with sametime running for some reason
+	#sametime is alive, because container will normally exit with sametime running for some reason
 	1)	sleep 1
 		;;
 	*) count=$(( $count + 1))
