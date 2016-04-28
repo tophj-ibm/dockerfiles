483d40c75a0193ee325e22ae57d28be502cbbe49
Christopher Jones <tophj@linux.vnet.ibm.com>
2016-04-28 13:59:54 -0500

    Updated sleep script

--- sametime/waitForStart.sh
+++ sametime/waitForStart.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
+#three should do it
 /usr/bin/sametime80
 count=0
-while [ $count -le 6 ]
+while [ $count -le 3 ]
 do
 	case "$(pidof sametime | wc -w)" in
 	#sametime is alive, because container will normally exit with sametime running for some reason
-	1)	sleep 1
+	1)	sleep 20
 		;;
 	*) count=$(( $count + 1))
-		sleep 1
+		sleep 20
 		;;
 	esac
 done
